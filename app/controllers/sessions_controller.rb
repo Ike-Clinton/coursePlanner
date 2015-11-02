a6a8be18f778cb27ec8d8ec2fa702b2230ba4ec6
rtumbusc <rtumbusc81@yahoo.com>
2015-11-02 21:17:39 +0000

    Tumbusch Fixed Session Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
   def login
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = 'Invalid User Email Given'
+      render 'new'
+    end
   end
 end
