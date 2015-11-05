7c95e423376adcccca76b7d1451746c2dce521e5
ike-clinton <ikeclinton@gmail.com>
2015-11-05 19:12:28 +0000

    ADDED route for post /register

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'courses#index'
    get 'register' => 'courses#register'
+   post 'register' => 'courses#register'
    get 'login' => 'courses#login'
    get 'main' => 'courses#main'
 
