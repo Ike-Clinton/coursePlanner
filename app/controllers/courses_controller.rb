dfab84f602d9315ae7a80deb2c2144deca15d83c
ike-clinton <ikeclinton@gmail.com>
2015-11-17 01:57:20 +0000

    Working on login code (fixed) minor bug

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -26,18 +26,15 @@ class CoursesController < ApplicationController
   end
   
   def register
+    
+  end
   
   def submit_login
       @user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:email])
-       # Log in the user and redirect to main page
-        log_in(user)
         redirect_to 
-      else 
         #Throws error if the email entered does not exists
         flash.now[:danger] = 'Invalid Email Entered'
         render 'new'
-      end
 
   end
   
