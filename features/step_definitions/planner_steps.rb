6afc1a09ad6b9802a8c0549b5c787ce78e032a9b
ike-clinton <ikeclinton@gmail.com>
2015-11-18 04:02:42 +0000

    Adding steps for authenticating users

--- features/step_definitions/planner_steps.rb
+++ features/step_definitions/planner_steps.rb
@@ -6,10 +6,24 @@ Given /the following user exists/ do |users_table|
   end
 end
 
-Given(/the following user is logged in$/) do |login_table|
-  login_table.hashes.each do |login|
-    log_in User.find_by login
-  end
+Given /^I have one\s+user "([^\"]*)" with email "([^\"]*)" and academic class "([^\"]*)" and is advisor "([^\"]*)"$/ do |name,email, academic_class, is_advisor|
+  @user = User.new(:email => email,
+                   :name=>name,
+                   :academic_class => academic_class,
+                   :is_advisor => is_advisor)
+   @user.save!
+end
+
+Given /^I am an authenticated user$/ do
+  name = 'exmample'
+  email = 'example@example.com'
+  password = 'secret!'
+
+  Given %{I have one user "#{name}" with email "#{email}" and password "#{password}"}
+  And %{I go to the user login page}
+  And %{I fill in "user_username" with "#{name}"}
+  And %{I fill in "user_password" with "#{password}"}
+  And %{I press "Sign in"}
 end
 
 Given /the following class_history exists/ do |class_table|
